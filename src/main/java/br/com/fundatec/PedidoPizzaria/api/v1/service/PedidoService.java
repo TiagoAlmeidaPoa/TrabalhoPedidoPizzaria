236f51899f99159899c67b28f66c580a3004b2df
TiagoAlmeidaPoa <tiago.pipinha@gmail.com>
2019-07-05 19:10:55 -0300

    criei PedidoService e PedidoRepository

--- src/main/java/br/com/fundatec/PedidoPizzaria/api/v1/service/PedidoService.java
+++ src/main/java/br/com/fundatec/PedidoPizzaria/api/v1/service/PedidoService.java
@@ -1,5 +1,23 @@
 package br.com.fundatec.PedidoPizzaria.api.v1.service;
 
+import org.springframework.stereotype.Service;
+
+import br.com.fundatec.PedidoPizzaria.api.v1.entity.Pedido;
+import br.com.fundatec.PedidoPizzaria.api.v1.repository.PedidoRepository;
+
+@Service
 public class PedidoService {
+	
+	private PedidoRepository pedidoRepository;
+
+	public PedidoService(PedidoRepository pedidoRepository) {
+		this.pedidoRepository = pedidoRepository;
+	}
+	
+	public Pedido consultar(Long id) {
+		return pedidoRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Não encontrou pedido para o id:" + id));
+	}
 
 }
+
